ea6f8cf6d43791db5041ed08c346596c59faf192
D-CHM260 <54440329+D-CHM260@users.noreply.github.com>
2019-08-23 13:31:17 +0100

    V1
    First Prototype

---
+++ Assets/Hex.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex{
+
+    public readonly int Col;
+    public readonly int Row;
+    public readonly int Aggregate;
+    float radius = 1f;
+
+    public Hex(int c, int r)
+    {
+        this.Col = c;
+        this.Row = r;
+        this.Aggregate = -(c + r);
+    }
+
+    public Vector3 position()
+    {
+        return new Vector3(
+            horiz() * (this.Col + this.Row / 2f),
+            0,
+            vert()* this.Row);
+    }
+
+    public float height()
+    {
+        return radius * 2;
+    }
+
+    public float width()
+    {
+        return height() * Mathf.Sqrt(3) / 2;
+    }
+
+    public float vert()
+    {
+       return height() * 0.75f;
+    }
+
+   public float horiz()
+    {
+        return width();
+    }
+
+
+
+
+
+}
---
+++ Assets/HexGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+	}
+
+    public GameObject HexPrefab;
+
+    public Material[] HexMaterials;
+
+    int dimensions = 25;
+
+    public void GenerateMap()
+    {
+        for(int column = 0; column < dimensions; column++)
+        {
+            for(int row = 0; row < dimensions; row++)
+            {
+                Hex h = new Hex(column, row);
+
+                GameObject hexGO = (GameObject)Instantiate(
+                    HexPrefab, h.position(), 
+                    Quaternion.identity, 
+                    this.transform);
+                MeshRenderer mr =hexGO.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
+                
+            }
+        }
+    }
+}
